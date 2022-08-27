6b1ea68c42cddf57c3ea941c5258ca7818e32d4c
alaanvv <valealan84@gmail.com>
2022-08-27 10:49:09 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+// DOM
+const muie = document.querySelector('.muie')
+const head = document.querySelector('.head')
+const body = document.querySelector('.body')
+const sniper = document.querySelector('.sniper')
+const arma = document.querySelector('.arma')
+const carregador = document.querySelector('.carregador img')
+const numbala = document.querySelector('.numbala')
+const boya = document.querySelector('.boya')
+const stats = document.querySelector('.stats p')
+
+// AUDIO
+const sndPow = new Audio('./assets/audio/shot.mp3')
+const sndNobru = new Audio('./assets/audio/nobru.mp3')
+const sndDamage = new Audio('./assets/audio/uf.mp3')
+const sndDeath = new Audio('./assets/audio/oof.mp3')
+const sndReload = new Audio('./assets/audio/reload.mp3')
+const sndClick = new Audio('./assets/audio/click.mp3')
+
+// VARIABLES
+let hp = 5
+let ammo = 10
+shooting = false
+let reloading = false
+let alive = (Math.random() * (3) + 2).toFixed()
+
+// FUNCTIONS
+
+function kill() {
+    playAudio(sndDeath)
+    
+    addRemove(muie, 'morto', 1500)
+    addRemove(muie, 'sumido', 2000, 1000)
+    addRemove(muie, 'walk', 2000, 3000)
+    
+    attStats()
+    hp = 5
+}
+
+function attStats() {
+    alive--
+    if (alive > 0) { stats.innerHTML = `Restam ${alive} vivos` }
+    else {
+        boya.style.visibility = 'visible'
+        playAudio(sndNobru)
+    }
+}
+
+function shoot(damage) {
+    if (shooting) return
+
+    shooting = true
+    setTimeout(e => { shooting = false }, 500)
+    
+    if (ammo > 0) ammo -= 1
+    numbala.innerText = ammo
+
+    hit(damage)
+    
+    if (ammo > 0) {
+        playAudio(sndPow)
+        addRemove(arma, 'recoil', 100)
+    } 
+    else {
+        playAudio(sndClick)
+        addRemove(arma, 'nobalas', 100)
+    }
+}
+
+function hit(damage) {
+    playAudio(sndPow)
+    hp -= damage
+
+    if (damage == 0) return
+    if (hp <= 0) return kill() 
+    playAudio(sndDamage)
+}
+
+function reload() {
+    reloading = true
+    playAudio(sndReload)
+    setTimeout(e => { reloading = false }, 1500)
+
+    addRemove(arma, 'reload', 1400)
+    addRemove(carregador, 'reload', 200)
+    setTimeout(() => { numbala.innerHTML = '0' }, 200)
+    setTimeout(() => { addRemove(arma, 'pulo', 300) }, 600)
+    setTimeout(e => {
+        ammo = 10
+        numbala.innerHTML = ammo
+    }, 1400)
+}
+
+function addRemove(dom, _class, time, delay = 0) {
+    setTimeout(e => {
+        dom.classList.add(_class)
+        setTimeout(e => { dom.classList.remove(_class) }, time)
+    }, delay)
+}
+
+function mapResizer() {
+    areas = document.querySelectorAll('area')
+
+    for (area of areas) { area.coordArr = area.coords.split(',') }
+    
+    function resizeMaps() {
+        scale = muie.offsetWidth / 472
+        for (area of areas) { area.coords = area.coordArr.map(coord => Math.round(coord * scale)).join(',') }
+    }
+    window.addEventListener('resize', () => resizeMaps())
+    resizeMaps()
+}
+
+function playAudio(audio) {
+    audio.currentTime = 0
+    audio.play()
+}
+
+// EVENT LISTENER
+carregador.addEventListener('click', e => reload(), false)
+document.addEventListener('keypress', e => { if (e.key == 'r') reload() })
+
+document.addEventListener('click', e => {
+    if (ammo > 0) { if (!reloading) shoot(0) }
+    else { addRemove(arma, 'nobalas', 100) }
+}, false)
+head.addEventListener('click', e => {
+    if (ammo > 0) { if (!reloading) shoot(5) }
+    else { addRemove(arma, 'nobalas', 100) }
+}, false)
+body.addEventListener('click', e => {
+    if (ammo > 0) { if (!reloading) shoot(1) }
+    else { addRemove(arma, 'nobalas', 100) }
+}, false)
+
+window.addEventListener('load', e => mapResizer(), false)
+
+// GERAL
+stats.innerHTML = `Restam ${alive} vivos`
